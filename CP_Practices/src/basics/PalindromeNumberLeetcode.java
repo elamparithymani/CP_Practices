131fa91346410eb3578cd2d8897f726a60ea9f05
Elas <Elas@Elas-PC>
2023-08-19 17:42:16 +0530

    initial commit

---
+++ CP_Practices/src/basics/PalindromeNumberLeetcode.java
@@ -0,0 +1,22 @@
+package basics;
+
+public class PalindromeNumberLeetcode {
+
+	public static void main(String args[]) {
+		System.out.println();
+		
+		int var,var2;
+		int x=123;
+        if(x>0) {
+            var = x%10;
+            var2 = x/10;
+            System.out.println(var);
+            System.out.println(var2);
+            var = x%10;
+            var2 = x/10;
+            System.out.println(var);
+            System.out.println(var2);
+        }
+	}
+
+}
--- CP_Practices/src/basics/TwoSum_Leetcode.java
+++ CP_Practices/src/basics/TwoSum_Leetcode.java
@@ -6,25 +6,24 @@ import java.util.Scanner;
 public class TwoSum_Leetcode {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
-	      System.out.println("Enter the length of the array:");
-	      int length = s.nextInt();
-	      int [] myArray = new int[length];
-	      System.out.println("Enter the elements of the array:");
+		System.out.println("Enter the length of the array:");
+		int length = s.nextInt();
+		int[] myArray = new int[length];
+		System.out.println("Enter the elements of the array:");
 
-	      for(int i=0; i<length; i++ ) {
-	         myArray[i] = s.nextInt();
-	      }
-	      System.out.println("Enter target element:");
-	      int target = s.nextInt();
-	      for(int i = 0; i<length ; i++) {
-	    	  for(int j = 0; j<length; j++) {
-	    		  if(myArray[i] + myArray[j] == target) {
-	    			  System.out.println("[" + myArray[i] + "] , ["+ myArray[j] + "]");
-	    		  }
-	    	  }
-	      }
+		for (int i = 0; i < length; i++) {
+			myArray[i] = s.nextInt();
+		}
+		System.out.println("Enter target element:");
+		int target = s.nextInt();
+		for (int i = 0; i < length; i++) {
+			for (int j = 0; j < length; j++) {
+				if (myArray[i] + myArray[j] == target) {
+					System.out.println("[" + myArray[i] + "] , [" + myArray[j] + "]");
+				}
+			}
+		}
 	}
 
 }
